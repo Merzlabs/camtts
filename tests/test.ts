1c179a92c98a90cfe59eb667fc7d62e98abba061
Niklas Merz <niklasmerz@linux.com>
2019-11-09 12:09:04 +0100

    Add tests for start functions

--- tests/test.ts
+++ tests/test.ts
@@ -6,14 +6,28 @@ const path = require('path');
 
 describe('camt.052', () => {
     let camt: string;
+    let report: AccountReport;
     
     beforeAll(() => {
         camt = fs.readFileSync(path.resolve(__dirname, 'test.xml'), 'utf-8')
     })
     
-    
     it('parse should not return a result', function () {
         const report = CAMT.parse(camt);
         expect(report).toBeDefined();
+    });
+
+
+    describe('camt.052', () => {
+        beforeEach(() => {
+            report = CAMT.parse(camt);
+        })
+
+        it('GroupHeader should be valid', function () {
+            expect(report.groupHeader).toBeDefined();
+            expect(report.groupHeader.messageId).toEqual('camt52_GR');
+            expect(report.groupHeader.creationDateTime).toEqual('2019-11-08T16:20:06.570Z');
+        });
+
     });
 });
